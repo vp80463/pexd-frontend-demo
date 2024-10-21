2395088b72234dc4c3a85ecce3ae6ab75ae8d65a
vp80463 <vp80463@globalymc.com>
2024-10-21 19:28:34 +0800

    [mid0589(chichun) on 20241021192136]

--- src/views/parts/purchase/spm0404_01.vue
+++ src/views/parts/purchase/spm0404_01.vue
@@ -5,7 +5,7 @@ import dayjs from 'dayjs';
 import { useRouter } from 'vue-router';
 import { useUser } from 'viy-menu';
 import { useApi } from '@/composables/useApi';
-import { CONST_SYSTEM_DATE_FORMAT, CONST_SYSTEM_DATE_VALUE_FORMAT, CONST_SYSTEM_TIME_VALUE_FORMAT } from '@/constants';
+import { CONST_SYSTEM_DATE_FORMAT, CONST_SYSTEM_TIME_VALUE_FORMAT } from '@/constants';
 import detailPagePage from '/src/views/parts/purchase/spm0404_02.vue';
 import { formatCodeInput, warn_msg } from '@/pj-commonutils.js';
 const { t } = useI18n();
@@ -562,8 +562,6 @@ const checkFilter = () => {
                     v-model="queryFormData.dateFrom"
                     type="date"
                     :style="{ width: '130px' }"
-                    :format="CONST_SYSTEM_DATE_FORMAT.ymd"
-                    :value-format="CONST_SYSTEM_DATE_VALUE_FORMAT.ymd"
                   />
                 </VueFormItem>
                 <!-- BEGIN CUSTOM DIV CODE -->
@@ -580,8 +578,6 @@ const checkFilter = () => {
                     v-model="queryFormData.dateTo"
                     type="date"
                     :style="{ width: '130px' }"
-                    :format="CONST_SYSTEM_DATE_FORMAT.ymd"
-                    :value-format="CONST_SYSTEM_DATE_VALUE_FORMAT.ymd"
                   />
                 </VueFormItem>
                 <VueButton id="viy2Button_hVBByI" ref="viy2Button_hVBByI" icon-position="left" class="icon-button-small" :icon="IconArrowLeftBold" @click="viy2Button_hVBByIClick" />
